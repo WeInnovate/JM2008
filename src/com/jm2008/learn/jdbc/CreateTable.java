f44ab1f614b15007c0c95d44b9acc9b6f810b468
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-07-29 10:06:52 +0530

    Create table

--- src/com/jm2008/learn/jdbc/CreateTable.java
+++ src/com/jm2008/learn/jdbc/CreateTable.java
@@ -1,5 +1,6 @@
 package com.jm2008.learn.jdbc;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,21 +8,17 @@ import java.sql.SQLException;
 public class CreateTable {
 
 	public static void main(String[] args) {
-
-
 		try {
 //			1. Establish the connection with database
 			Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-			
-			if(con != null) {
-				System.out.println("Connection created....");
-			}
-			else {
-				System.out.println("Connection not created...");
-			}
 //			2. Create statement/command
+			Statement stmt = con.createStatement();
 //			3. execute the command
+			boolean retVal = stmt.execute("CREATE TABLE JM2008_USER (ID NUMBER, NAME VARCHAR)");
 //			4. Process the result
+			if (!retVal) {
+				System.out.println("Table created successfully!");
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
