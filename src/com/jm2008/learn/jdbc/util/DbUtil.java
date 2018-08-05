754329bce8b41ee8ea2be5277cd41548395b02c1
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-08-05 10:25:47 +0530

    externalized the db properties

--- src/com/jm2008/learn/jdbc/util/DbUtil.java
+++ src/com/jm2008/learn/jdbc/util/DbUtil.java
@@ -3,14 +3,29 @@ package com.jm2008.learn.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class DbUtil {
 
 	private static Connection con;
 
+	private static String url;
+
+	private static String userName;
+
+	private static String password;
+
+	static {
+		ResourceBundle rb = ResourceBundle.getBundle("db", Locale.US);
+		url = rb.getString("url");
+		userName = rb.getString("userName");
+		password = rb.getString("password");
+	}
+
 	public static Connection getCon() throws SQLException {
 		if (con == null) {
-			return DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+			return DriverManager.getConnection(url, userName, password);
 		}
 		return con;
 	}
