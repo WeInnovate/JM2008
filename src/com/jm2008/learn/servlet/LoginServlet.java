dcc64a896427623daebe06cba3f4673c40f4bbe0
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-09-13 12:02:53 +0530

    servlet

---
+++ src/com/jm2008/learn/servlet/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.jm2008.learn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/LoginServlet", "/login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("LoginServlet doGet()");
+		response.sendRedirect("login.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("LoginServlet doPost()");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println(username);
+		if (username.equalsIgnoreCase(password)) {
+//			PrintWriter out = response.getWriter();
+//			out.println("Hello, " + username);
+			RequestDispatcher rd = request.getRequestDispatcher("profile");
+			rd.forward(request, response);
+		} else {
+			response.sendRedirect("https://www.google.com?q=audi");
+//			response.sendRedirect("index.html");
+		}
+	}
+
+}
---
+++ src/com/jm2008/learn/servlet/RegisterServlet.java
@@ -0,0 +1,75 @@
+package com.jm2008.learn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public RegisterServlet() {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("registration.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/HTML");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String mobile = request.getParameter("mobile");
+		String age = request.getParameter("age");
+		String gender = request.getParameter("gender");
+		String[] courses = request.getParameterValues("courses");
+		String city = request.getParameter("city");
+		String comment = request.getParameter("comment");
+
+		PrintWriter out = response.getWriter();
+		out.println(name);
+		out.println(email);
+		out.println(mobile);
+		out.println(age);
+		out.println(gender);
+
+		for (int i = 0; i < courses.length; i++) {
+			out.println(courses[i]);
+		}
+
+		out.println(city);
+		out.println(comment);
+		out.println("<br />");
+		out.println("<hr />");
+		out.print("<table class='table'>\r\n" + 
+				"  <thead>\r\n" + 
+				"    <tr>\r\n" + 
+				"      <th scope='col'>Attribute</th>\r\n" + 
+				"      <th scope='col'>Value</th>\r\n" + 
+				"    </tr>\r\n" + 
+				"  </thead>\r\n" + 
+				"  <tbody>\r\n" + 
+				"    <tr>\r\n" + 
+				"      <th scope='row'>Name</th>\r\n" + 
+				"      <td>"+name+"</td>\r\n" + 
+				"    </tr>\r\n" + 
+				"    <tr>\r\n" + 
+				"      <th scope='row'>Email</th>\r\n" + 
+				"      <td>"+email+"</td>\r\n" + 
+				"    </tr>\r\n" + 
+				"    <tr>\r\n" + 
+				"      <th scope='row'>Mobile</th>\r\n" + 
+				"      <td>"+mobile+"</td>\r\n" + 
+				"    </tr>\r\n" + 
+				"  </tbody>\r\n" + 
+				"</table>");
+	}
+
+}
